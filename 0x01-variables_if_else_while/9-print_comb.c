ee786069c7ca73e62dd287e775f40eb76d9819aa
Gonhis <ismail012348@gmail.com>
2023-03-18 15:21:19 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 100; n++)
+	{
+		putchar(n + '0');
+		if (n < 99)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}

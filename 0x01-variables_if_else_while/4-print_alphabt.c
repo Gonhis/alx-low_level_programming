513f2d230c2dc5ab3fc5dd9d3f7733af24302b90
Gonhis <ismail012348@gmail.com>
2023-03-18 04:35:51 -0700

    4-print_alphabt.c done

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char al;
+
+	al = 'a';
+	for (al = 'a'; al <= 'z' && al != 'q' 'e'; al++)
+	{
+		putchar(al);
+	}
+	putchar('\n');
+	return (0);
+}

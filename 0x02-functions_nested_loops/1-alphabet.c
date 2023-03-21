b153ceff21a9a2d21f1bc4464c7b27b1a7d5f15d
Gonhis <ismail012348@gmail.com>
2023-03-21 08:14:12 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+
+void print_alphabet(void);
+{
+	char lc;
+
+	lc = 'a';
+	for (lc = 'a'; lc <= 'z'; lc++)
+	{
+		_putchar(lc);
+	}
+	_putchar('\n);
+	return (0);
+}

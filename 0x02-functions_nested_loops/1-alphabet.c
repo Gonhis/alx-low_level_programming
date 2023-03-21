2c613cb995d88ead0825c9a1407c79bbb9ace03e
Gonhis <ismail012348@gmail.com>
2023-03-21 08:21:05 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,7 +5,7 @@
  * Return: 0
  */
 
-void print_alphabet(void);
+void print_alphabet(void)
 {
 	char lc;
 
@@ -14,6 +14,6 @@ void print_alphabet(void);
 	{
 		_putchar(lc);
 	}
-	_putchar('\n);
+	_putchar('\n');
 	return (0);
 }

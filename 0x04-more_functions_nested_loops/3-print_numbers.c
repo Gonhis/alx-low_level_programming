88a4bb892507e363b69706730e943204d6c1343d
Gonhis <ismail012348@gmail.com>
2023-03-23 05:09:31 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,17 +2,15 @@
 /**
  * print_numbers - prints the numbers, from 0 to 9
  *
- * Return: always 0
+ * Return: no return
  */
 void print_numbers(void)
 {
 	int i;
 
-	i = 0;
-	while (i >= 0 && i <= 9)
+	for (i = 48; i <= 57; i++)
 	{
-		_putchar(i + 48);
-		i++;
+		_putchar(i);
 	}
 	_putchar('\n');
 }

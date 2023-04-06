b1b8bea7810e402848acaff17ad138d42a6975b0
Gonhis <ismail012348@gmail.com>
2023-04-06 08:43:29 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,4 +1,7 @@
 #include "main.h"
+
+int new_sqrt_recursion(int n, int i);
+
 /**
  * _sqrt_recursion - returns the natural square root of a number
  *
@@ -14,18 +17,18 @@ int _sqrt_recursion(int n)
 	}
 	else
 	{
-		return (nat_squ_ro(n, 0));
+		return (new_sqrt_recursion(n, 0));
 	}
 }
 /**
- * nat_squ_ro - checks the natural squarte root
+ * new_sqrt_recursion - checks the natural squarte root
  *
  * @n: input number
  * @i: squarte numb
  *
  * Return: natural squarte root number or -1 error
  */
-int nat_squ_ro(int n, int i)
+int new_sqrt_recursion(int n, int i)
 {
 	if (i * i == n)
 	{
@@ -37,6 +40,6 @@ int nat_squ_ro(int n, int i)
 	}
 	else
 	{
-		return (nat_squ_ro(n, i + 1));
+		return (new_sqrt_recursion(n, i + 1));
 	}
 }

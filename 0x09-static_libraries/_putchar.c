07d0b65289846dc3bff49571ac34f8ee6707dc0c
Gonhis <ismail012348@gmail.com>
2023-04-09 10:47:40 -0700

    libmy.a

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,1 +1,12 @@
-int _putchar(char c);
+#include <unistd.h>
+/**
+ * _putchar : write the character c 
+ *
+ * @c: the character to input
+ *
+ * Return: 1 or -1
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}

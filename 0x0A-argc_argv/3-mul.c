491df82d1c9ee7002fb1baa0d6b80950422b4893
Gonhis <ismail012348@gmail.com>
2023-04-10 09:18:53 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * main - multiplies two numbers
  *
@@ -9,23 +10,17 @@
  */
 int main(int argc, char *argv[])
 {
-	int i, mult;
+	int mult;
 
-	printf("%d\n", argc);
-	if (argc > 1 && argc < 4)
+	if (argc == 3)
 	{
-		for (i = 1; i < argc; i++)
-		{
-			printf("argv[%d] = %s\n", i, argv[i]);
-			mult = atoi(argv[i]) * atoi(argv[i]);
-		}
+		mult = atoi(argv[1]) * atoi(argv[2]);
 		printf("Total = %d\n", mult);
 	}
 	else
 	{
 		printf("Error\n");
+		return (1);
 	}
-	return (1);
+	return (0);
 }
-
-

1d46c4598d8a8786578a6727060ca26a226b2235
Gonhis <ismail012348@gmail.com>
2023-04-14 16:49:43 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,32 +13,38 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *p;
 
-	int i, j, s1_len, s2_len;
+	unsigned int i, s1_len, s2_len;
 
 	if (s1 == NULL)
-		s1 = "";
+	{
+		s1_len = 0;
+	}
+	else
+	{
+		for (s1_len = 0; s1[s1_len] != '\0'; ++s1_len)
+			;
+	}
 	if (s2 == NULL)
-		s2 = "";
-
-	for (s1_len = 0; s1[s1_len] != '\0'; s1_len++)
-		;
-	for (s2_len = 0; s2[s2_len] != '\0'; s2_len++)
-		;
-
-	p = malloc(s1_len + n + 1);
+	{
+		s2_len = 0;
+	}
+	else
+	{
+		for (s2_len = 0; s2[s2_len] != '\0'; ++s2_len)
+			;
+	}
+	if (s2_len > n)
+		s2_len = n;
+	p = malloc(sizeof(char) * (s1_len + s2_len + 1));
 	if (p == NULL)
 	{
 		return (NULL);
 	}
 
-	for (i = 0; s1[i] != '\0'; i++)
+	for (i = 0; i < s1_len; i++)
 		p[i] = s1[i];
-	for (j = 0; j < n; j++)
-	{
-		p[i] = s2[j];
-		i++;
-	}
-
-	p[i] = '\0';
+	for (i = 0; i < s2_len; i++)
+		p[i + s1_len] = s2[i];
+	p[s1_len + s2_len] = '\0';
 	return (p);
 }

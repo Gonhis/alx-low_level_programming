b59c50ddb2e8a039f0ce43a82252e0071f1fd8f1
Gonhis <ismail012348@gmail.com>
2023-04-17 18:51:49 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - new type of struct dog
+ *
+ * @name: dog name
+ * @age: dog age
+ * @owner: owner name
+ */
+struct dog
+{
+	char *name;
+
+	float age;
+
+	char *owner;
+};
+#endif

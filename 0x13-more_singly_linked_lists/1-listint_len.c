cacb0238d963d8d368e792232733ae7f09f41b7a
Gonhis <ismail012348@gmail.com>
2023-05-01 07:02:39 -0700

    1-listint_len.c

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * listint_len - function that returns the number of elements in a
+ * linked listint_t list
+ *
+ * @h: head node of listint_t
+ *
+ * Return: number of elements
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t A = 0;
+
+	for (; h != NULL; h = h->next)
+	{
+		A++;
+	}
+	return (A);
+}

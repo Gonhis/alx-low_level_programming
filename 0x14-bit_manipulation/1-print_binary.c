6dcb05fe36ea43fd15f17f2bed3bf294993e997e
gonhis <h11mh12is@gmail.com>
2023-08-08 07:14:22 -0700

    done

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+* print_binary - function that prints the binary representation of a number
+*
+* @n: number to print in binary
+*
+* Return: empty
+*/
+void print_binary(unsigned long int n)
+{
+	int index = 63, C = 0;
+
+	unsigned long int ism;
+
+	while (index >= 0)
+	{
+		ism = n >> index;
+		if (ism & 1)
+		{
+			_putchar('1');
+			C++;
+		}
+		else if (C)
+		{
+			_putchar('0');
+		}
+		index--;
+	}
+	if (!C)
+	{
+		_putchar('0');
+	}
+}
---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+* flip_bits - function that returns the number of bits you would need
+* to flip to get from one number to another
+*
+* @n: number 1
+* @m: number 2
+*
+* Return: number of bits
+*/
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int index = 63, C = 0;
+
+	unsigned long int ism;
+
+	unsigned long int XOR_op = n ^ m;
+
+	while (index >= 0)
+	{
+		ism = XOR_op >> index;
+		if (ism & 1)
+		{
+			C++;
+		}
+		index--;
+	}
+	return (C);
+}
